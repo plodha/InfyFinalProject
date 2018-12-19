5f5b8c3c92fc3ca063011f5964dce518be0603c5
Hailun Yu <hailun.yu@ad.infosys.com>
2018-12-19 11:48:19 -0500

    Inserted Values for Computer_Purchase_Details

--- BackEnd/src/com/infy/resources/PCPicker_TableScripts_v3.0.sql
+++ BackEnd/src/com/infy/resources/PCPicker_TableScripts_v3.0.sql
@@ -55,23 +55,23 @@ CREATE TABLE Computer_Purchase_Details(
 
 
 
-INSERT INTO PCUser VALUES ('1001','Jane#Cat','Jane Catfish','jane.cat@infosys.com','8695748321','230');
+INSERT INTO PCUser VALUES ('1001','Jane#Cat','Jane Catfish','jane.cat@infosys.com','8695748321','430');
 INSERT INTO PCUser VALUES ('1002','Bale#Don','Bale Donut','bale.don@infosys.com','9584738576','745');
 INSERT INTO PCUser VALUES ('1003','Gale#Glo','Gale Glow','gale.gloc25@infosys.com','5860396849','386');
 INSERT INTO PCUser VALUES ('1004','Sunj#Pra','Sunjay Pranav','sunj.pra08@infosys.com','2960439628','850');
-INSERT INTO PCUser VALUES ('1005','Chri#Pal','Christine Pale','chri.pal@infosys.com','9584766751','1');
-INSERT INTO PCUser VALUES ('1006','Dest#Joh','Destiny John','dest.joh@infosys.com','1069228395','100');
+INSERT INTO PCUser VALUES ('1005','Chri#Pal','Christine Pale','chri.pal@infosys.com','9584766751','510');
+INSERT INTO PCUser VALUES ('1006','Dest#Joh','Destiny John','dest.joh@infosys.com','1069228395','500');
 INSERT INTO PCUser VALUES ('1007','Hele#Hac','Helen Hack','hele.hac@infosys.com','5504987362','804');
 INSERT INTO PCUser VALUES ('1008','Chet#Poo','Chetan Pool','chet.poo@infosys.com','6079382758','669');
 
-INSERT INTO Boat_Booking_Details VALUES ('5001','1005','2003','05-MAR-2017','09-MAR-2017',3996,7788991234,null);
-INSERT INTO Boat_Booking_Details VALUES ('5002','1002','2004','06-MAR-2017','08-MAR-2017',1998,9945451230,0.05);
-INSERT INTO Boat_Booking_Details VALUES ('5003','1005','2005','09-MAR-2017','10-MAR-2017',999,9898457612,0.10);
-INSERT INTO Boat_Booking_Details VALUES ('5004','1006','2006','12-MAY-2017','16-MAY-2017',3996,7788678901,0.10);
-INSERT INTO Boat_Booking_Details VALUES ('5005','1003','2007','10-MAY-2017','13-MAY-2017',2997,9087147820,null);
-INSERT INTO Boat_Booking_Details VALUES ('5006','1001','2008','05-JUN-2017','06-JUN-2017',1199,9933123089,0.10);
-INSERT INTO Boat_Booking_Details VALUES ('5007','1007','2001','05-SEP-2017','06-SEP-2017',699,7760981423,null);
-INSERT INTO Boat_Booking_Details VALUES ('5008','1004','2002','08-SEP-2016','10-SEP-2017',2398,7788993322,0.10);
+INSERT INTO Computer_Purchase_Details VALUES ('1','0','1003','16-APR-2018', 400, 5860396849);
+INSERT INTO Computer_Purchase_Details VALUES ('2','1','1004','06-MAR-2018', 350, 2960439628);
+INSERT INTO Computer_Purchase_Details VALUES ('3','2','1005','09-MAR-2018', 200, 9584766751);
+INSERT INTO Computer_Purchase_Details VALUES ('4','3','1006','12-MAY-2016', 1002, 1069228395);
+INSERT INTO Computer_Purchase_Details VALUES ('5','4','1007','10-MAY-2018', 1250, 5504987362);
+INSERT INTO Computer_Purchase_Details VALUES ('6','5','1008','05-JUN-2016', 935, 6079382758);
+INSERT INTO Computer_Purchase_Details VALUES ('7','6','1001','05-SEP-2016', 653, 8695748321);
+INSERT INTO Computer_Purchase_Details VALUES ('8','5','1002','08-SEP-2017', 935, 9584738576);
 
 select * from PC_Details;
 select * from PCUser;
